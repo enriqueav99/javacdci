81c896ccd99fd73e89358135b467ccadf8da3ca9
Administrador <Administrador@MacBooktrador68.home>
2023-05-17 12:55:43 +0200

    cambio de cluster

--- kubernetes/prometheus.sh
+++ kubernetes/prometheus.sh
@@ -1,5 +1,4 @@
-cd kube-prometheus
-kubectl create -f manifests/setup
-kubectl create -f manifests/
+kubectl create -f kube-prometheus/manifests/setup
+kubectl create -f kube-prometheus/manifests/
 kubectl -n monitoring delete networkpolicies.networking.k8s.io --all
 kubectl --namespace monitoring patch svc grafana -p '{"spec": {"type": "LoadBalancer"}}'
