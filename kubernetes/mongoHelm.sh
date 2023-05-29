62d9b79721340bf0060945cb95284bf4a1acae9f
Administrador <Administrador@MacBooktrador68.home>
2023-05-29 03:34:50 +0200

    act

--- kubernetes/mongoHelm.sh
+++ kubernetes/mongoHelm.sh
@@ -1,2 +1,3 @@
+helm repo add mongodb https://mongodb.github.io/helm-charts
 helm install mongodbc-chart mongodb/community-operator --namespace nsmongo --create-namespace
 kubectl config set-context --current --namespace=nsmongo
