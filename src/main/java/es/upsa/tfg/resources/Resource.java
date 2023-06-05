c6812c861a0107750d007a9a2030ab7cf06febf4
Administrador <Administrador@MacBooktrador68.home>
2023-06-05 02:49:14 +0200

    cambios app

--- src/main/java/es/upsa/tfg/resources/Resource.java
+++ src/main/java/es/upsa/tfg/resources/Resource.java
@@ -36,6 +36,7 @@ public class Resource {
             summary = "Acceso a los datos de todos los productos",
             description = "Devuelve los datos de los productos"
     )
+    /*
     @APIResponses({
             @APIResponse(responseCode = "200",
                     description = "Se han localizado los productos y se devuelven sus datos",
@@ -45,7 +46,7 @@ public class Resource {
             @APIResponse(responseCode = "500",
                     description = "Error al recuperar productos"
             )
-    })
+    })*/
 
     @GET
     @Path("/todos")
