16c07184374feda09e5ae9ddf847085b09616f83
Administrador <Administrador@MacBooktrador68.home>
2023-06-06 00:19:52 +0200

    actualizaciones

--- kubernetes/prometheus.sh
+++ kubernetes/prometheus.sh
@@ -1,4 +1,5 @@
 kubectl create -f kube-prometheus/manifests/setup
 kubectl create -f kube-prometheus/manifests/
 kubectl -n monitoring delete networkpolicies.networking.k8s.io --all
+
 kubectl --namespace monitoring patch svc grafana -p '{"spec": {"type": "LoadBalancer"}}'
