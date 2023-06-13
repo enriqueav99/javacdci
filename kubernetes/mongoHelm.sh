ec083de4e07f627bbbff2f89697fc00bb34553d4
Administrador <Administrador@MacBooktrador68.home>
2023-06-13 21:03:21 +0200

    actualizaciones

--- kubernetes/mongoHelm.sh
+++ kubernetes/mongoHelm.sh
@@ -1,5 +1,6 @@
 helm repo add mongodb https://mongodb.github.io/helm-charts
 helm install mongodbc-chart mongodb/community-operator --namespace nsmongo --create-namespace
 kubectl config set-context --current --namespace=nsmongo
+
 #Esperar a que se instale el controlador(chart) para desplegar el recurso:
 # kubectl apply -f ./mongoCRD.yml
